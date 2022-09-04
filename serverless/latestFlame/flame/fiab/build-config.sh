aab2f800f97a95e956e07d42d0ce439da70ec437
gaurav25 <gaurav25@localhost>
2022-09-04 13:22:30 -0600

    Modified jobs.json

--- serverless/latestFlame/flame/fiab/build-config.sh
+++ serverless/latestFlame/flame/fiab/build-config.sh
@@ -16,7 +16,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Assumption: apiserver ingress doesn't have tls enabled. Hence, http is used in config.yaml below.
-apiserver_host=$(kubectl get ing -n flame  | grep flame-apiserver | awk '{print $3}')
+apiserver_host="flame-apiserver.flame.test:30371"
 
 FLAME_DIR=$HOME/.flame
 
